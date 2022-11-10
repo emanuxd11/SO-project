2aedf36239fad9f147ce19cbe120085ba444a84e
Emanuel Maia <tavanomanu@gmail.com>
2022-11-10 22:16:36 +0000

    Tá mais ou menos o primeiro :/

--- samples.c
+++ samples.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
-// Só para testar, no final a seed deve ser 0
-#include <time.h>
-
-#define LEN 7 + 1
-#define N_SAMPLES 5
 
 typedef struct {
     int size;
     int *elems;
 } vector;
 
+/*
+    Retorna -2 em caso de erro, o índice 
+    em que se encontra o caracter indesejado, 
+    caso este exista, ou -1 caso não haja nenhum
+    erro nem nenhum caracter indesejado.
+*/
 int check_unwanted(char *line) {
     if (line == NULL) {
-        return EXIT_FAILURE;
+        return -2;
     }
 
-    for(int i = 0; i < sizeof(line); i++) {
+    for(int i = 0; i < strlen(line); i++) {
         if (line[i] == '\n') {
-            return 1;
-        }
+            line[i] = ' ';
+            return i;
+        }    
     }
 
-    return 0;
+    return -1;
 }
 
+/*
+    Retorna -1 em caso de erro, 0 caso 
+    o valor inserido já exista no vetor,
+    ou 1 caso não exista. 
+*/
 int check_repeated(vector v, int elem) {
     if (v.elems == NULL || v.size < 0) {
-        return EXIT_FAILURE;
+        fprintf(stderr, "Error: invalid vector");
+        return -1;
     }
 
     if (v.size == 0) {
@@ -37,7 +48,6 @@ int check_repeated(vector v, int elem) {
 
     for(int i = 0; i < v.size; i++) {
         if (v.elems[i] == elem) {
-            // printf("\n%d", elem);
             return 1;
         }
     }
@@ -45,46 +55,93 @@ int check_repeated(vector v, int elem) {
     return 0;
 }
 
+/*
+    Retorna o tamanho do ficheiro ou -1
+    em caso de erro.
+*/
 int get_file_size(char *filename) {
     struct stat file_stats;
     if (stat(filename, &file_stats) == -1) {
-        return EXIT_FAILURE;
+        perror("Error");
+        return -1;
     }
 
     return file_stats.st_size;
 }
 
 int main(int argc, char *argv[]) {
-    srandom(time(NULL));
-    FILE *fp;
-    // provisório
-    char *filename = "quote.txt";
-    fp = fopen(filename, "r");
+    if (argc < 4) {
+        fprintf(stderr, "%s: missing arguments", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-    char line[LEN];
+    srandom(0);
+    FILE *fp;
+    char *filename = argv[1];
+    int n_samples = atoi(argv[2]), sample_len = atoi(argv[3]) + 1;
+
+    if ((fp = fopen(filename, "r")) == NULL) {
+        fprintf(stderr, "Error: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    char line[sample_len];
     int jump;
     vector jumps;
-    jumps.elems = malloc(sizeof(int) * N_SAMPLES);
+
+    int file_size = get_file_size(filename);
+    if (file_size == -1) {
+        return EXIT_FAILURE;
+    }
+
+    jumps.elems = malloc(sizeof(int) * n_samples);
     jumps.size = 0;
+    if (jumps.elems == NULL) {
+        fprintf(stderr, "Error: couldn't allocate memory: malloc");
+        return EXIT_FAILURE;
+    }
 
-    for(int i = 0; i < N_SAMPLES; i++) {
+    for(int i = 0; i < n_samples; i++) {
         while(1) {
-            jump = random() % (get_file_size(filename) - LEN);
-            fseek(fp, jump, SEEK_SET);
-            fgets(line, LEN, fp);
-
-            if (check_repeated(jumps, jump) == 0 && 
-            check_unwanted(line) == 0) {
+            jump = random() % (file_size - sample_len);
+            
+            int status = check_repeated(jumps, jump);
+            if (status == 0) {
                 break;
+            } else if (status == -1) {
+                free(jumps.elems);
+                return EXIT_FAILURE;
             }
         }
+        
+        while(1) {
+            fseek(fp, jump, SEEK_SET);
+            fgets(line, sample_len, fp);
 
+            int pos = check_unwanted(line);
+            if (pos == -1) {
+                break;
+            }
+            if (pos == -2) {
+                free(jumps.elems);
+                fprintf(stderr, "Error: invalid line");
+                return EXIT_FAILURE;
+            }
+
+            char next_line[sample_len];
+            fgets(next_line, sample_len - (pos + 1), fp);
+            strcat(line, next_line);
+            
+            break;
+        }
+        
         printf(">%s<\n", line);
 
         jumps.elems[i] = jump;
         jumps.size++;
     }
 
+    free(jumps.elems);
     fclose(fp);
     return EXIT_SUCCESS;
 }
