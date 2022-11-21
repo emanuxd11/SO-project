8a04bc1b360ee9956925858861757462947a928e
Emanuel Maia <tavanomanu@gmail.com>
2022-11-21 21:20:05 +0000

    O 2 já está

--- q2/txt2epub.c
+++ q2/txt2epub.c
@@ -13,25 +13,51 @@ int main(int argc, char *argv[]) {
     char new_ext[CMDLEN];
     char *target_name = "ebooks.zip";
 
-    int i;
-    for (i = 1; i < argc; i++) {
-        strncpy(old_ext, argv[i], sizeof(char) * strlen(argv[i]));
-        strncpy(new_ext, strtok(argv[i], "."), sizeof(char) * strlen(argv[i]) + 1);
-        strncat(new_ext, ".epub", sizeof(char) * strlen(argv[i]) + 1);
+    for (int i = 1; i < argc; i++) {
+        // para não usar strncpy e strncat, acho que é melhor 
+        // verificar antes se o tamanho não é demasiado grande
+        if (strlen(argv[i]) > CMDLEN - 2) {
+            fprintf(stderr, "file number %d won't be" 
+            "added because filename is too large", i);
+            continue;
+        }
 
+        // aqui já seria seguro usar o strcpy e strcat
+        // porque já garantimos que a memória é suficiente
+        strcpy(old_ext, argv[i]);
+        strcpy(new_ext, strtok(argv[i], "."));
+        strcat(new_ext, ".epub");
+
+        // primeiro fork para executar o pandoc
         if ((pid = fork()) == 0) {
             if (execlp("pandoc", "pandoc", old_ext, "-o", new_ext, NULL) == -1) {
                 perror("execlp");
                 exit(EXIT_FAILURE);
             }
         } else {
-            waitpid(pid, NULL, 0);
+            // o processo pai espera que o processo
+            // filho termine a execução do pandoc
+            if (waitpid(pid, NULL, 0) < 0) {
+                fprintf(stderr, "%s: waitpid error: %s\n",
+                argv[0], strerror(errno));
+            }
         }
 
+        // primeiro fork para executar o zip
         if ((pid = fork()) == 0) {
-            execlp("zip", "zip", target_name, new_ext, NULL);
+            if (execlp("zip", "zip", target_name, new_ext, NULL) == -1) {
+                perror("execlp");
+                exit(EXIT_FAILURE);
+            }
         } else {
-            waitpid(pid, NULL, 0);
+            // o processo pai espera que o processo
+            // filho termine a execução do pandoc
+            if (waitpid(pid, NULL, 0) < 0) {
+                fprintf(stderr, "%s: waitpid error: %s\n",
+                argv[0], strerror(errno));
+            }
         }
     }
+
+    exit(EXIT_SUCCESS);
 }
