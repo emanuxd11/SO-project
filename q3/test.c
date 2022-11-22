5c7f9683bbdf0e8443e6500421a78a79f2d60967
Emanuel Maia <tavanomanu@gmail.com>
2022-11-22 00:42:24 +0000

    Adicionei um ficheiro ao q3 que explica a ordem dos FIFOs

---
+++ q3/test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int main (int argc, char *argv[]) {
+    if (mkfifo("myfifo", 0755) == -1) {
+        if (errno != EEXIST) {
+            fprintf(stderr, "Could not create new fifo file!\n");
+            return 1;
+        }
+    }
+    
+    int pid; 
+    if((pid = fork()) < 0) {
+        perror("fork");
+    } else if (pid == 0) {
+        printf("READING - Opening...\n");
+        int fd = open("myfifo", O_RDONLY);
+        printf("READING - Opened\n");
+        int x;
+        printf("READING - Reading...\n");
+        if (read(fd, &x, sizeof(x)) == -1) {
+            return 3;
+        }
+        printf("READING - Read, value of x is %d\n", x);
+        printf("READING - Closing...\n");
+        close(fd);
+        printf("READING - Closed\n");
+
+    } else {
+        printf("WRITING - Opening...\n");
+        int fd = open("myfifo", O_WRONLY);
+        printf("WRITING - Opened\n");
+        int x = 97;
+        printf("WRITING - Writing...\n");
+        if (write(fd, &x, sizeof(x)) == -1) {
+            return 2;
+        }
+        printf("WRITING - Wrote\n");
+        printf("WRITING - Closing...\n");
+        close(fd);
+        printf("WRITING - Closed\n");
+    }
+
+    return 0;
+}
---
+++ q3/tokenring.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main (int argc, char *argv[]) {
+    
+}
