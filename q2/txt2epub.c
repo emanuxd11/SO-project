17ba9cecc7dd1cb2a9485e59afd9692ae78e6149
Emanuel Maia <tavanomanu@gmail.com>
2022-11-22 14:32:23 +0000

    Fiz umas cenas no 3 e alterei o 2 para executar os processos em paralelo.

--- q2/txt2epub.c
+++ q2/txt2epub.c
@@ -9,55 +9,80 @@
 
 int main(int argc, char *argv[]) {
     pid_t pid;
+
+    // para guardar o nome do ficheiro com a
+    // antiga extensão e com a nova extensão
     char old_ext[CMDLEN];
     char new_ext[CMDLEN];
-    char *target_name = "ebooks.zip";
 
-    for (int i = 1; i < argc; i++) {
-        // para não usar strncpy e strncat, acho que é melhor 
-        // verificar antes se o tamanho não é demasiado grande
+    // alocar espaço para argc ficheiros, o nome do zip e o próprio comando
+    char **epub_files;
+    epub_files = malloc((argc + 2) * sizeof(char *));
+
+    // inicialização dos primeiros dois elementos do array de comandos
+    epub_files[0] = "zip";
+    epub_files[1] = "ebooks.zip";
+
+    // ciclo dos forks
+    for (int i = 1, j = 2; i < argc; i++, j++) {
         if (strlen(argv[i]) > CMDLEN - 2) {
             fprintf(stderr, "file number %d won't be" 
             "added because filename is too large", i);
             continue;
         }
 
-        // aqui já seria seguro usar o strcpy e strcat
-        // porque já garantimos que a memória é suficiente
         strcpy(old_ext, argv[i]);
         strcpy(new_ext, strtok(argv[i], "."));
         strcat(new_ext, ".epub");
+        epub_files[j] = malloc(CMDLEN * sizeof(char));
+        strcpy(epub_files[j], new_ext);
 
-        // primeiro fork para executar o pandoc
-        if ((pid = fork()) == 0) {
+        if ((pid = fork()) < 0) {
+            // em caso de erro
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // processo filho
             if (execlp("pandoc", "pandoc", old_ext, "-o", new_ext, NULL) == -1) {
                 perror("execlp");
                 exit(EXIT_FAILURE);
             }
         } else {
-            // o processo pai espera que o processo
-            // filho termine a execução do pandoc
-            if (waitpid(pid, NULL, 0) < 0) {
-                fprintf(stderr, "%s: waitpid error: %s\n",
-                argv[0], strerror(errno));
-            }
+            // a função do processo pai é criar mais
+            // processos filhos que executem o pandoc
+            continue;
         }
+    }
+    epub_files[argc + 1] = NULL;
 
-        // primeiro fork para executar o zip
-        if ((pid = fork()) == 0) {
-            if (execlp("zip", "zip", target_name, new_ext, NULL) == -1) {
-                perror("execlp");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            // o processo pai espera que o processo
-            // filho termine a execução do pandoc
-            if (waitpid(pid, NULL, 0) < 0) {
-                fprintf(stderr, "%s: waitpid error: %s\n",
-                argv[0], strerror(errno));
-            }
+    // ciclo dos waits
+    for (int i = 1; i < argc; i++) {
+        if (wait(NULL) < 0) {
+            fprintf(stderr, "%s: wait error: %s\n",
+            argv[0], strerror(errno));
         }
     }
 
+    // execução do zip
+    if ((pid = fork()) < 0) {
+        // em caso de erro
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // o processo filho executa o comando zip
+        if (execvp("zip", epub_files) == -1) {
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        // o processo pai espera que o 
+        // filho termine de executar
+        if (waitpid(pid, NULL, 0) < 0) {
+            fprintf(stderr, "%s: waitpid error: %s\n",
+            argv[0], strerror(errno));
+        }
+    }
+
+    // exit gracefully xD
     exit(EXIT_SUCCESS);
 }
