6e42780ed10784b2f2ebd38d75fb49304425487b
Emanuel Maia <tavanomanu@gmail.com>
2022-11-23 23:35:22 +0000

    A borga máxima está quase terminada

--- q3/tokenring.c
+++ q3/tokenring.c
@@ -8,10 +8,22 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <time.h>
 
 #define NAME_LEN 256
 
-unsigned int x;
+int token = 0;
+int fd_write;
+int fd_read;
+
+int __roda_do_preco_certo_69(double p) {
+    double random = (double)rand() / RAND_MAX;
+    if (random <= p) {
+        return 1;
+    }
+
+    return 0;
+}
 
 int main (int argc, char *argv[]) {
     if (argc < 4) {
@@ -23,14 +35,20 @@ int main (int argc, char *argv[]) {
         argc = 4;
     }
 
-    unsigned int n = atoi(argv[1]);
+    int n = atoi(argv[1]);
+    if (n < 2) {
+        fprintf(stderr, "%s: need at least 2 pipes, %d given", argv[0], n);
+        exit(EXIT_FAILURE);
+    }
     double p = atof(argv[2]);
     int t = atoi(argv[3]);
 
-    /* make the fifos */
+    // make the fifos
     char fifoname[NAME_LEN];
     char **fifoarr;
-    fifoarr = malloc((n + 1) * sizeof(char *));
+    fifoarr = malloc(n * sizeof(char *));
+
+    pid_t pid;
 
     for (unsigned int i = 1, j = 0; i <= n; i++, j++) {
         if (i == n) {
@@ -50,44 +68,57 @@ int main (int argc, char *argv[]) {
         fifoarr[j] = malloc((strlen(fifoname) + 1) * sizeof(char));
         strcpy(fifoarr[j], fifoname);
     }
-    fifoarr[n] = malloc((strlen(fifoarr[0]) + 1) * sizeof(char));
-    strcpy(fifoarr[n], fifoarr[0]);
 
-    pid_t pid;
-    int fd_write;
-    int fd_read;
-        
     for (unsigned int i = 1, j = 0; i <= n; i++, j++) {
         if ((pid = fork()) < 0) {
             perror("fork");
+            exit(EXIT_FAILURE);
         }
-        
+
+        srand(getpid());
+
         if (pid == 0) {
-            // Child Process
-            if ((fd_write = open(fifoarr[j + 1], O_WRONLY)) < 0) {
-                perror("fd_write error");
-            }
-            printf("[pid%d] opened %s for writing\n", getpid(), fifoarr[j + 1]);
-
+            //Child Process
             if (i == 1) {
-                x = -1;
-                write(fd_write, &x, sizeof(x));       
-                printf("[pid%d] wrote x = %u\n", getpid(), x);
-            }
+               if ((fd_read = open(fifoarr[0], O_RDONLY)) < 0) {
+                   perror("fd_read error");
+               }
+               if ((fd_write = open(fifoarr[1], O_WRONLY)) < 0) {
+                   perror("fd_write error");
+               }
+               write(fd_write, &token, sizeof(int));
+            } else if(i == n) {
+                puts("opening for write");
+                if((fd_write = open(fifoarr[0],O_WRONLY)) < 0) {
+                    perror("fd_write error");
+                }
+                puts("opening for read");
+                if((fd_read = open(fifoarr[j], O_RDONLY)) < 0) {
+                   perror("fd_read error");
+                }
+            } else {
+                if ((fd_write = open(fifoarr[j+1],O_WRONLY)) < 0) {
+                    perror("fd_write error");
+                }
 
+                if ((fd_read = open(fifoarr[j], O_RDONLY)) < 0) {
+                    perror("fd_read error");
+                }
+            }
+            
             for ( ; ; ) {
-                read(fd_read, &x, sizeof(x));
-                printf("[pid%d] x = %u\n", getpid(), x);
-                sleep(5);
-                x++;
-                write(fd_write, &x, sizeof(x));
+                read(fd_read, &token, sizeof(token));
+
+                if (__roda_do_preco_certo_69(p)) {
+                    printf("[p%d] lock on token (val = %d)\n", i, token);
+                    sleep(t);
+                    printf("[p%d] unlocked token\n", i);
+                }
+
+                token++;
+                write(fd_write, &token, sizeof(token));
             }
         }
-
-        if ((fd_read = open(fifoarr[j], O_RDONLY)) < 0) {
-            perror("fd_read error");
-        }
-        printf("[pid%d] opened %s for reading\n", getpid(), fifoarr[j]);
     }
 
     for (unsigned int i = 0; i < n; i++) {
